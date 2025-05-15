deec2fa51a9375db75349af11bd0720906540929
Subhajit685 <subhajit09111999@gmail.com>
2025-05-15 12:59:26 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,84 @@
+import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+
 function App() {
-  return <h1>This is my First AWS MERN Deploy</h1>;
+  const [users, setusers] = useState([]);
+  const [name, setname] = useState("");
+  const [email, setemail] = useState("");
+
+  const getuser = async () => {
+    try {
+      const res = await fetch(`http://13.60.85.176/api/user`, {
+        method: "GET",
+        credentials: "include",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+
+      const data = await res.json();
+      console.log(data);
+      if (data.success) {
+        setusers(data.user);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const submit = async (e) => {
+    e.preventDefault();
+    const data = {
+      name,
+      email,
+    };
+    try {
+      const res = await fetch(`http://13.60.85.176/api`, {
+        method: "POST",
+        credentials: "include",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
+
+      const value = await res.json();
+      if (value.success) {
+        setusers(value.user, ...users);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getuser();
+  }, []);
+  return (
+    <div>
+      <h1>Your name and email</h1>
+      <label htmlFor="">Name</label>
+      <input
+        type="text"
+        placeholder="name"
+        onChange={(e) => setname(e.target.value)}
+      />
+      <label htmlFor="">Email</label>
+      <input
+        type="text"
+        placeholder="email"
+        onChange={(e) => setemail(e.target.value)}
+      />
+      <button onClick={submit}>Submit</button>
+      <div>
+        {users &&
+          users.map((user) => {
+            return <div>Hi</div>;
+          })}
+      </div>
+    </div>
+  );
 }
 
 export default App;
